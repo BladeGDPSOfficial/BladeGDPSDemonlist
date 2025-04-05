73de44e929b50063c73da50bfcdd80ce5ea92cc9
cinderrblock <103962472+cinderrblock@users.noreply.github.com>
2025-04-05 20:41:53 -0300

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,9 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import Timemachine from './pages/Timemachine.sj';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/timemachine', component: Timemachine },
 ];
